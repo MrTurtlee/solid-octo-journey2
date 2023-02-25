bee310b89d983cc19d2d6b87e17fb61c044d3eda
Bonnie Coutts <bonnie@192-168-1-103.tpgi.com.au>
2023-02-25 15:26:42 +1100

    added a simplemath file

---
+++ intro/simplemath.py
@@ -0,0 +1,19 @@
+# it is printinfg the math calculation
+print (2+2)
+
+# it prints everything in " "
+print("2+2")
+
+# " " = string
+# string is a bunch of characters that aren't supposed to be calculated
+
+# example of interger and sting not adding
+# print(1 + "1")
+
+print (10-2)
+
+# [shift 8] - the sharp symbol is a mulitplication symbol
+print (12*10)
+
+# the symbol: / is for dividing
+print (20/10)
