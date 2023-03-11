f67c99811a1d02322875b12bf06e0f5d96e7b5b6
Beau Coutts <Beau.coutts@gmail.com>
2023-03-11 15:31:31 +1100

    added inputmath.py

---
+++ intro/inputmath.py
@@ -0,0 +1,8 @@
+import math # borrows someone else's math code
+print("Pythagoras Theorem calculator ")
+a = int(input("How big is side a"))
+b = int(input("how big is side b"))
+
+c = math.sqrt(a*a + b*b)
+print("The hypotenus is", c)
+
