333c31a16976a7df7fb8260bc774eff37b7fa3f0
Beau Coutts <Beau.coutts@gmail.com>
2023-03-18 14:58:10 +1100

    Added condition.py

---
+++ intro/condition.py
@@ -0,0 +1,24 @@
+age = 10
+
+# if statement recieves a comparison and runs the code inside it
+# if the comparison is true.
+if (age == 10):
+    print("Your age is", age)
+
+# if the comparison is false, and we want to check it with another
+# Comparison, we can use elif to check for a different condition
+elif (age < 10):
+    print("You are underaged")
+
+# True and False are called booleans
+
+# if my age > 10, print "you are overaged"
+elif (age > 10):
+   print("You are overaged")
+
+# for greater equals / less than equals, the comparitors are:
+# >= and <= ,  respectively
+
+# != represents not equals to
+if (age != 5):
+    print("nice")
