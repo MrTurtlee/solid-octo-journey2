498bac046297c72ecbbd9083fd464946134f82f5
Beau Coutts <Beau.coutts@gmail.com>
2023-03-18 15:22:43 +1100

    Added inputcondition.py

---
+++ intro/inputcondition.py
@@ -0,0 +1,13 @@
+User_reply = input("Do you like robots?  [Type yes or no]")
+
+
+if User_reply == "yes":
+    print ("Beep Boop")
+elif User_reply == "no":    
+    print("well, robots dont like you either.")
+elif User_reply == "Maybe":
+    print("Make up your mind, human.")
+elif User_reply == "Only ones with laser eyes":
+    print("Zzzap")
+else:
+    print("Your human nonsense offends us.")
