8d231c6531153838c8b1e232bb8244fd3c96f758
Beau Coutts <Beau.coutts@gmail.com>
2023-04-01 15:29:47 +1100

    Started guessing game

--- Games/Guessing.py
+++ Games/Guessing.py
@@ -2,7 +2,9 @@ import random
 
 print("Guess a number between 1 and 20")
 guess_Number = int(input("What number do you choose?"))
-if guess_Number == random.randint(1,20):
+
+number = random.randint(1,20)
+if guess_Number == number:
     print("What number do you choose?")
 
 
