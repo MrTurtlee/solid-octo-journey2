0982dda6230b4ce4291c118fdeb02a906405e8f0
Beau Coutts <Beau.coutts@gmail.com>
2023-04-01 14:02:36 +1100

    Added a new door to dragonsnax

--- Games/Dragonsnax.py
+++ Games/Dragonsnax.py
@@ -1,9 +1,9 @@
 import random
 
 print("You are in a dark room in a mysterious castle")
-print("In front of you are 3 doors. You must choose one.")
+print("In front of you are 4 doors. You must choose one.")
 
-player_choice = input("Choose 1, 2 or 3\n")
+player_choice = input("Choose 1, 2, 3 or 4\n")
 
 if player_choice == "1":
     print("You find a room full of treasure. You're rich!")
@@ -39,7 +39,19 @@ elif player_choice =="3":
             print("GAME OVER, YOU LOSE!")
     else:
         print("Sorry, you didn't enter 1 or 2")
-else:
-    print("Sorry, you didn't enter 1, 2, or 3")
+elif: player_choice =="4":
+    print("You enter a room with a sphinx")
+    print("It asks you to guess what number it is thinking of, between 1 and 10")
+
+    sphinx_number = int(input("What number do you choose?"))
+    if: number == random.randint(1,10)
+    if: sphinx_number <= 4:
+        print("The sphinx hisses in disapointment and lets you go past")
+        print("GAME OVER, YOU WIN!")
+    elif: print("The sphinx tells you that your guess is incorrect")
+        print("You are now her captive forever")  
+        print("GAME OVER, YOU LOSE!")         
+    
+else: print("Sorry, you didn't enter 1, 2, 3 or 4")
 
 print("Run the game again to have another go.")
