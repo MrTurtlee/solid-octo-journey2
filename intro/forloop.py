6bed62c31a33b9774f90965bddf7eea301d543f0
Beau Coutts <Beau.coutts@gmail.com>
2023-04-29 15:30:39 +1000

    Made stuf on nestedloops.py

--- intro/forloop.py
+++ intro/forloop.py
@@ -4,5 +4,7 @@
 #     print(x)
 #     x = x + 1
 
+# for loop which counts every number in the range (0,10)
+# print y for every number 'y' is at
 for y in range(0,10):
     print(y)
---
+++ intro/nestedloops.py
@@ -0,0 +1,14 @@
+# count = 0
+# for x in range(0,10):
+#     for y in range(0,10):
+#         count = count + 1
+#         print(count)
+
+# print(count)        
+        
+# maths times table
+
+for y in range(0,13):
+    print("7 x ", y, " = ", 7*y)
+
+
