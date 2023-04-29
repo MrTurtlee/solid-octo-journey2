6a25cc18d49af861926eae2bd206b5f5ffd6809a
Beau Coutts <Beau.coutts@gmail.com>
2023-04-29 15:02:02 +1000

    Finished guessing.py

--- Games/Guessing.py
+++ Games/Guessing.py
@@ -1,14 +1,19 @@
 import random
 
 print("Guess a number between 1 and 20")
-guess_Number = int(input("What number do you choose?"))
+guess_Number = random.randint(1,20)
+guess = -1
 
-number = random.randint(1,20)
-if guess_Number == number:
-    print("What number do you choose?")
+while guess != guess_Number:
+    guess = int(input("What number do you choose?"))
 
-
-
+    number = random.randint(1,20)
+    if guess == number:
+        print("You guessed the right number")
+    if guess < guess_Number:    
+        print("Your guess is too low")
+    if guess > guess_Number:
+        print("Your guess is too high")
 
 
 
