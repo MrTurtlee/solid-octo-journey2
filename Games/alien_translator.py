4513c2af7db5878cf6175d17796b586adc2674d6
Beau Coutts <Beau.coutts@gmail.com>
2023-06-17 15:30:33 +1000

    made alien_translator.py

---
+++ Games/alien_translator.py
@@ -0,0 +1,30 @@
+
+alienDictionary = {
+    "we": "vorag",
+    "come": "thang",
+    "in": "zon",
+    "peace": "argh",
+    "hello": "kodar",
+    "can": "znak",
+    "i": "az",
+    "borrow": "liftit", 
+    "some": "zum", 
+    "rocket": "upgoman", 
+    "fuel": "kakboom", 
+    "please": "slepin", 
+    "don't": "baaaaaaaaaarn", 
+    "shoot": "flabil", "welcome": "unkip", 
+    "our": "mandig", 
+    "new": "brang", 
+    "alien": "marangin", 
+    "overlords": "bap"
+}
+
+# code logic:
+
+english = input("Type in an english phrase\n")
+english.lower().split()
+
+for word in english:
+    print(word)
+
