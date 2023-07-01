28be8de81ef948d30fd82dc2889ba22b42114c98
Beau Coutts <Beau.coutts@gmail.com>
2023-07-01 14:45:51 +1000

    Made function.py

---
+++ intro/function.py
@@ -0,0 +1,13 @@
+# Functions is a term used to represent blocks of code with a 
+# title attached to it.
+
+# to make a function, use: def name() :
+def printing_hello ():
+    print ("hello")
+
+printing_hello()
+
+# ne coding terms:
+# - function
+# - perameters => represent the temporary variables within
+# the function brackets
