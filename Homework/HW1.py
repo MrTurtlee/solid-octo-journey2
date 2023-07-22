230f1c322dd5d75306dcac1e6cfbbfde1dc02e2f
Beau Coutts <Beau.coutts@gmail.com>
2023-07-22 15:21:46 +1000

    made homework folder

---
+++ Homework/HW1.py
@@ -0,0 +1,12 @@
+# Choose a mathmatical equation / formular and code it so that it gives a
+#result
+
+# for eg:
+    # You can choose a pythagoras theorem, which gets 2 numbers from
+    # the user and print out the result
+
+# If you  make your own equation, make 3 of them
+# for eg: y = x^2 + 30
+
+# if you are using square root, you can use 'import maths',
+# and google how to use sqrt
