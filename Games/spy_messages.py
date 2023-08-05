ef7aa70507329c2f7df148bf27f9ef7039a4f27a
Beau Coutts <Beau.coutts@gmail.com>
2023-08-05 14:34:13 +1000

    hm

--- Games/spy_messages.py
+++ Games/spy_messages.py
@@ -13,5 +13,16 @@ shift_amount = int(input("Please enter a whole number from 1-25 to be your key")
 encrpted_string = ""
 
 for letter in string_to_encrypt:
-    print (letter)
+    # first get the position of the letter in the alphabet
+    # using .find(letter)
 
+
+    # make a new_position variable which is the
+    # sum of the given shift_amount and the found position amount
+
+    # for now, print the letter in the alphabet with the new_position
+    # using your knowledge that strings are technically lists
+
+    
+
+    print (letter)
