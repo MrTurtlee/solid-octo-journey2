d7ba7cdec342d8baa5537f9e4cfed00e4fe2dd21
Beau Coutts <Beau.coutts@gmail.com>
2023-08-05 15:30:12 +1000

    made functions V2

---
+++ intro/functionsV2.py
@@ -0,0 +1,10 @@
+# Functions
+# functions are like a box to keep your code in
+
+# they are created like this:
+# def = function
+# print_sentance = function name
+# () = parameters
+
+def print_sentance():
+    print("hello world")
