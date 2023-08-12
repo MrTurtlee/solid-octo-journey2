94802d2d8d4dbb05afa4c3fe21156ba4aa3c2d5f
Beau Coutts <Beau.coutts@gmail.com>
2023-08-12 14:46:49 +1000

    hm

--- intro/functionsV2.py
+++ intro/functionsV2.py
@@ -8,3 +8,31 @@
 
 def print_sentance():
     print("hello world")
+
+print_sentance()
+
+def print_parameters(param1, param2):
+    print(param1)
+    print(param2)
+
+print_parameters("hello", "world")
+
+# the parameters are temporary variables that are used only in 
+# the function
+
+
+# usage of global variable:
+def print_bananans():
+    print(num_bananas)
+
+num_bananas = 2
+
+print_bananans()
+
+def multiply_bananas():
+    # update the variable to 3 times the original num_bananas
+    num_bananas = 3*num_bananas
+    print_bananans()
+
+
+
