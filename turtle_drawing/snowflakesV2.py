136dd6e229bd5410b27ac1e327e32b75c5e03919
Beau Coutts <Beau.coutts@gmail.com>
2023-09-16 15:21:50 +1000

    did random stuff

--- turtle_drawing/snowflakesV2.py
+++ turtle_drawing/snowflakesV2.py
@@ -1,37 +1,47 @@
 from turtle import*
 
-shape("turtle")
-speed(10)
-pencolor("white")
-pensize(4)
-Screen().bgcolor("turquoise")
 
 # forward(num)   - this one moves the turtle forward by a certain num of steps
 # right(num)     - turns the turtle to the right by a certain num of degrees
 # left(num)      - turns the turtle to the right by a certain num of degrees
 # backward(num)  - this one moves the turtle backward by a certain num of steps
 
-# 1st arm
-arm_length = 30
-def v_shape():    
+
+def v_shape(v_length, num_v):    
     right(25)
-    forward(50)
-    backward(50)
+    forward(v_length)
+    backward(v_length)
     left(50)
-    forward(50)
-    backward(50)
+    forward(v_length)
+    backward(v_length)
     right(25)
 
-def arm():
-    for i in range(0,4):
+def arm(arm_length, v_length, num_v):
+    for i in range(0, num_v):
         forward(arm_length)
-        v_shape()
-    backward(arm_length * 4)
+        v_shape(v_length, num_v)
+    backward(arm_length * num_v)
 
 # for entire snowflake
-for i in range(0,6):
-    arm()
-    left(60)
+def snowflake(num_arms, arm_length, v_length, num_v):
+    for i in range(0,num_arms):
+        arm(arm_length, v_length, num_v)
+        left(360 / num_arms)
 
+
+def main():
+    shape("turtle")
+    speed(500)
+    pencolor("white")
+    pensize(4)
+    Screen().bgcolor("turquoise")
+    arm_length = 30
+    v_length = 50
+    num_arms = 10
+    num_v = 4
+
+    snowflake(num_arms, arm_length, v_length, num_v)
+
+main()
 
 done()
