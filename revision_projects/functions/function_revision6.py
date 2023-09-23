3d572c1c45b5f88c092c3e1f52aefbcb42db39ee
Beau Coutts <Beau.coutts@gmail.com>
2023-09-23 15:29:48 +1000

    eh

---
+++ revision_projects/functions/function_revision6.py
@@ -0,0 +1,16 @@
+
+
+# make a program which if your number is divisible by 11
+
+
+# 1) getting the input and converting it to an int
+
+# 2) do some if conditions to check if it is devisible
+#       - use % to help check
+#           example: 10 % 3 == 0
+#           because 10 / 2 == 5 with no remainder
+#               so % gives 0 (the remainder)
+
+# 3) print a final comment on whether the number is divisible or not
+
+user_input = input()
