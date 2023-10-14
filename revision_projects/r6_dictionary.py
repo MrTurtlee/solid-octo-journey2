3586b23b2f277ab0b819529abb2af31cfa857b5a
Beau Coutts <Beau.coutts@gmail.com>
2023-10-14 15:30:47 +1100

    did a bunch of stuff

---
+++ revision_projects/r6_dictionary.py
@@ -0,0 +1,31 @@
+# Task:
+# create a dictionary consisting
+# of the following data:
+superman: "kryptonite",
+batman: "nothing",
+flash: "cold temperatures",
+aquaman: "needs to touch water every hour",
+
+
+# then create the following story:
+
+# print dictionary 
+# "Superman: Why do you have a record of our weaknesses"
+# "Batman: For safety (goes on with a detailed explanation)"
+# "Flash: Hey, why do you have no weakness? Let me fix that for you"
+# fix batman's value to "parents"
+# print dictionary
+# "Aquaman: Now that's a weakness"
+# "(Batman quietly fumes)"
+# delete batman entry in dictionary
+# print dictionary
+# "Flash: that's cheating"
+# "Batman: I will have my vengeance."
+# "Batman: [print flash's value], is quite a weakness too"
+
+weakness = {
+    "superman": "kryptonite"
+    "batman": "nothing"
+    "flash": "cold temperatures"
+    "aquaman": "needs to touch water every hour"
+}
