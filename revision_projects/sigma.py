2b21270ae3bb564762f01e4f8176c9305c4606ba
Beau Coutts <Beau.coutts@gmail.com>
2023-11-25 14:40:04 +1100

    made sigma.py (not the dude)

---
+++ revision_projects/sigma.py
@@ -0,0 +1,37 @@
+# question 1:
+total = 0
+for n in range(1,6):
+    total = total + (2*n - 3)
+print(total)
+
+
+# Q3
+total = 0
+for i in range(1,6):
+    total = total + (i**2)
+print(total)
+
+
+# Q4
+total = 0
+for n in range(1,6):
+    total = total + ((n**2 + 4) / (2*n))
+print(total)
+
+# Q5
+total = 0
+for n in range (1,6):
+    total = total + (n)
+print(total)
+
+# Q6
+total = 0
+for n in range(1,6):
+    total = total + (n**2)
+print(total)
+
+# Q7
+total = 0 
+for n in range(1,6):
+    total = total + ((-1)**n * (2**n))
+print(total)
