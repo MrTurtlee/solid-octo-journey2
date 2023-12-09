39620099d405e0a6fd7426d5db63a1c8e8e5132b
Beau Coutts <Beau.coutts@gmail.com>
2023-12-09 14:39:49 +1100

    did hw

---
+++ Homework/HW6.py
@@ -0,0 +1,8 @@
+# make a for loop which goes through numbers from 1 to 1 million
+# print every number that is divisible by 5 or 7
+
+for i in range(1, 10000001):
+    if i % 5 == 0:
+        print(i)
+    elif i % 7 == 0:
+        print(i)
