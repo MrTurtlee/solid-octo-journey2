a2e684d79033c37f319ff6520862d422fb0fa79b
Beau Coutts <Beau.coutts@gmail.com>
2024-04-30 17:28:53 +1000

    did some revision

---
+++ Tuesday/revision/Revison1.py
@@ -0,0 +1,33 @@
+# 1:
+print("Hi Mr Christian")
+
+# 2:
+print(193 * 253)
+
+# 3:
+given_age = input("What is your age?")
+print("You are", given_age, "years old")
+
+# 4:
+given_num = int(input("Give me a number"))
+print(given_num + 15)
+
+# 5:
+numbers = 1
+while numbers < 11:
+    print(numbers)
+    numbers = numbers + 1
+
+# 6:
+num_list = [2,4,6,8,10,12,14,16,18,20]
+list_position = 0
+while list_position < 10:
+    print(num_list[list_position])
+    list_position = list_position + 1
+
+# 7:
+MyList = ["Hi ", "My ", "Name " , "is ", "Beau ", "Coutts"]
+sentence = ""
+for j in MyList:
+    sentence = sentence + j
+print(sentence)
